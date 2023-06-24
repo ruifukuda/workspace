2e62adcb1be686ad5066a9a2e57a9e011cb45935
ruifukuda <rufk222@gmail.com>
2023-06-24 11:58:25 +0900

    教科書終了

---
+++ Sukkiri/src/Code17_1.java
@@ -0,0 +1,29 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Code17_1 {
+	public static void main(String[] args) {
+		/*
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter("data.txt");
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		} finally {
+			if (fw != null) {
+				try {
+					fw.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		*/
+		try(FileWriter fw = new FileWriter("data");){
+			fw.write("hello");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Sukkiri/src/Ex17_2.java
@@ -0,0 +1,17 @@
+
+public class Ex17_2 {
+
+	public static void main(String[] args) {
+		String s = null;
+		try {
+			System.out.println(s.length());
+		} catch (Exception e) {
+			System.out.println("NullPointerException例外をcatchしました");
+			System.out.println("--スタックトレース(ここから)--");
+			e.printStackTrace();
+			System.out.println("--スタックトレース(ここまで)--");
+		}
+
+	}
+
+}
